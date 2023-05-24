9c6c6db2339f8de75e8c32ba8e796e1f453bc5fa
Nuril <korshunval2033@gmail.com>
2023-05-24 21:50:59 +0300

    :)

--- app/player2/page.tsx
+++ app/player2/page.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
   title: "Milord",
   description: "Test SSR",
   openGraph: {
+    title: "Milord Video",
     images: {
       url: "http://miyulibackend.pp.ua/api/photo/68",
       secureUrl: "https://miyulibackend.pp.ua/api/photo/68",
@@ -18,6 +19,7 @@ export const metadata: Metadata = {
       secureUrl: "https://miyulibackend.pp.ua/api/video/stream/1",
       width: 1280,
       height: 720,
+      type: "video/mpeg",
     },
     type: "video.other",
   },
