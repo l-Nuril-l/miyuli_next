d8e6d78a3f81d8e3d5ca46d9ed0246838284ff92
Nuril <korshunval2033@gmail.com>
2023-05-24 22:49:43 +0300

    4

---
+++ app/milord3/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import { FunctionComponent } from "react";
+
+interface PlayerPageProps {}
+
+export const metadata: Metadata = {
+  title: "Milord",
+  description: "Test SSR",
+  openGraph: {
+    title: "Milord Video",
+    type: "video.other",
+    url: "/milord",
+    videos: {
+      url: "http://h.zipdox.net/thursday/embed.webm",
+      secureUrl: "https://h.zipdox.net/thursday/embed.webm",
+      width: 1280,
+      height: 720,
+      type: "text/html",
+    },
+  },
+};
+
+const PlayerPage: FunctionComponent<PlayerPageProps> = () => {
+  return (
+    <>
+      <video controls src="http://h.zipdox.net/thursday/embed.webm"></video>
+    </>
+  );
+};
+
+export default PlayerPage;
