e34b4133650980b7388070550e15bf5f4ffb54e2
Nuril <korshunval2033@gmail.com>
2023-05-31 03:35:36 +0300

    .

---
+++ app/milord5/page.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next";
+import { FunctionComponent } from "react";
+
+interface PlayerPageProps {}
+
+export const metadata: Metadata = {
+  title: "Milord",
+  description: "Test SSR",
+  openGraph: {
+    title: "Milord Video",
+    type: "video.other",
+    url: "/milord5",
+    images: {
+      url: "http://miyulibackend.pp.ua/api/photo/7",
+      secureUrl: "https://miyulibackend.pp.ua/api/photo/7",
+    },
+    videos: {
+      url: "http://miyulibackend.pp.ua/api/video/stream2/5.mp4",
+      secureUrl: "https://miyulibackend.pp.ua/api/video/stream2/5.mp4",
+      width: 1280,
+      height: 720,
+      type: "text/html",
+    },
+  },
+};
+
+const PlayerPage: FunctionComponent<PlayerPageProps> = () => {
+  return (
+    <>
+      <video controls src="http://miyulibackend.pp.ua/api/video/stream2/2.mp4"></video>
+    </>
+  );
+};
+
+export default PlayerPage;
