637df2120a671cff22397bcad302b959af147446
Nuril <korshunval2033@gmail.com>
2023-05-31 05:09:25 +0300

    Head

--- app/milord/page.tsx
+++ app/milord/page.tsx
@@ -1,7 +1,13 @@
 import { Metadata } from "next";
+import Head from "next/head";
 import { FunctionComponent } from "react";
 
-interface PlayerPageProps {}
+interface PlayerPageProps {
+  data: {
+    title: string;
+    description: string;
+  };
+}
 
 export const metadata: Metadata = {
   title: "Milord",
@@ -11,12 +17,12 @@ export const metadata: Metadata = {
     type: "video.other",
     url: "/milord",
     images: {
-      url: "http://miyulibackend.pp.ua/api/video/thumbnail/48",
-      secureUrl: "https://miyulibackend.pp.ua/api/video/thumbnail/48",
+      url: "http://miyulibackend.pp.ua/api/video/thumbnail/1",
+      secureUrl: "https://miyulibackend.pp.ua/api/video/thumbnail/1",
     },
     videos: {
-      url: "http://miyulibackend.pp.ua/api/video/stream/48.mp4",
-      secureUrl: "https://miyulibackend.pp.ua/api/video/stream/48.mp4",
+      url: "http://miyulibackend.pp.ua/api/video/stream/1.mp4",
+      secureUrl: "https://miyulibackend.pp.ua/api/video/stream/1.mp4",
       width: 1280,
       height: 720,
       type: "text/html",
@@ -24,12 +30,30 @@ export const metadata: Metadata = {
   },
 };
 
-const PlayerPage: FunctionComponent<PlayerPageProps> = () => {
+const PlayerPage: FunctionComponent<PlayerPageProps> = ({ data }) => {
   return (
     <>
-      <video controls src="https://miyulibackend.pp.ua/api/video/stream/48"></video>
+      <Head>
+        <meta name="title" content={data.title} />
+        <meta name="og:title" content={"og" + data.title} />
+        <meta name="description" content={"og" + data.description} />
+        <meta name="og:description" content={data.description} />
+        <meta name="og:video:url" content="http://miyulibackend.pp.ua/api/video/stream/1.mp4" />
+        <meta name="og:video:secure_url" content="https://miyulibackend.pp.ua/api/video/stream/1.mp4" />
+      </Head>
+      <video controls src="https://miyulibackend.pp.ua/api/video/stream/1"></video>
     </>
   );
 };
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://miyulibackend.pp.ua/api/video/1`);
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
 
 export default PlayerPage;
