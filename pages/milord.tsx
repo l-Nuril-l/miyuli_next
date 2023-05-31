eb0344ff8a40feda7608a8c449b2460d7fc5bace
Nuril <korshunval2033@gmail.com>
2023-05-31 07:10:39 +0300

    test2

--- pages/milord.tsx
+++ pages/milord.tsx
@@ -40,11 +40,20 @@ const PlayerPage: NextPage<PlayerPageProps> = ({ video }) => {
     <>
       <Head>
         <meta name="title" content={video.title} />
-        <meta name="og:title" content={"og" + video.title} />
-        <meta name="description" content={"og" + video.description} />
+        <meta name="description" content={video.description} />
+        <meta name="og:title" content={video.title} />
         <meta name="og:description" content={video.description} />
+        <meta name="og:type" content="video.other" />
+        <meta name="og:url" content="/milord" />
+        <meta name="og:image:url" content={`http://miyulibackend.pp.ua/api/video/thumbnail/${video.id}`} />
+        <meta name="og:image:secure_url" content={`https://miyulibackend.pp.ua/api/video/thumbnail/${video.id}`} />
+        <meta name="og:video:width" content="1280" />
+        <meta name="og:video:height" content="720" />
+        <meta name="og:video:url" content={`http://miyulibackend.pp.ua/api/video/stream/${video.id}.mp4`} />
+        <meta name="og:video:secure_url" content={`https://miyulibackend.pp.ua/api/video/stream/${video.id}.mp4`} />
+        <meta name="og:video:type" content="text/html" />
       </Head>
-      <video controls src="https://miyulibackend.pp.ua/api/video/stream/1"></video>
+      <video controls src={`https://miyulibackend.pp.ua/api/video/stream/${video.id}`}></video>
     </>
   );
 };
--- pages/pvz.tsx
+++ pages/pvz.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { GetServerSideProps, Metadata } from "next";
+import Head from "next/head";
 import { FunctionComponent } from "react";
 
 interface PlayerPageProps {
@@ -39,7 +40,22 @@ export async function generateMetadata({ video }: PlayerPageProps): Promise<Meta
 const PlayerPage: FunctionComponent<PlayerPageProps> = ({ video }) => {
   return (
     <>
-      <video controls src="https://miyulibackend.pp.ua/api/video/stream/5"></video>
+      <Head>
+        <meta name="title" content={video.title} />
+        <meta name="description" content={video.description} />
+        <meta name="og:title" content={video.title} />
+        <meta name="og:description" content={video.description} />
+        <meta name="og:type" content="video.other" />
+        <meta name="og:url" content="/milord" />
+        <meta name="og:image:url" content={`http://miyulibackend.pp.ua/api/video/thumbnail/${video.id}`} />
+        <meta name="og:image:secure_url" content={`https://miyulibackend.pp.ua/api/video/thumbnail/${video.id}`} />
+        <meta name="og:video:width" content="1280" />
+        <meta name="og:video:height" content="720" />
+        <meta name="og:video:url" content={`http://miyulibackend.pp.ua/api/video/stream/${video.id}.mp4`} />
+        <meta name="og:video:secure_url" content={`https://miyulibackend.pp.ua/api/video/stream/${video.id}.mp4`} />
+        <meta name="og:video:type" content="text/html" />
+      </Head>
+      <video controls src={`https://miyulibackend.pp.ua/api/video/stream/${video.id}`}></video>
     </>
   );
 };
