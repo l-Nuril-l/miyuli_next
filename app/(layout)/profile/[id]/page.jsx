b0839fd1eb28fefa43d3b263f58beec001f69958
Nuril <korshunval2033@gmail.com>
2024-10-24 20:29:47 +0300

    test

--- app/(layout)/profile/[id]/page.jsx
+++ app/(layout)/profile/[id]/page.jsx
@@ -1,18 +1,18 @@
 import Profile from '@/pages_app/Profile';
-import { MiyuliService } from '@/services/miyuli.service';
 import React from 'react';
 
-export async function generateMetadata({ params, searchParams }) {
-    const profile = await MiyuliService.getAccount((await params).id);
-    return {
-        title: "MIYULI | " + profile.name + ' ' + profile.surname,
-        // ...(await metadataExtractor((await searchParams).z))
-    }
-}
+// export async function generateMetadata({ params, searchParams }) {
+//     const profile = await MiyuliService.getAccount((await params).id);
+//     return {
+//         title: "MIYULI | " + profile.name + ' ' + profile.surname,
+//         ...(await metadataExtractor((await searchParams).z))
+//     }
+// }
 
 export default async function Page({ params }) {
-    const profile = await MiyuliService.getAccount((await params).id);
+    // const profile = await MiyuliService.getAccount((await params).id);
     return (
-        <Profile profile={profile} />
+        // <Profile profile={profile} />
+        <Profile />
     )
 }
