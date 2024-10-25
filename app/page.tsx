19d3e7d001ddf36beadec1b23c629796ca2c3192
Nuril <korshunval2033@gmail.com>
2024-10-26 00:03:12 +0300

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default async function Home() {
   const cookiesStore = await cookies();
-  redirect(cookiesStore.has("auth") ? "feed" : "login");
+  permanentRedirect(cookiesStore.has("auth") ? "feed" : "/");
 }
